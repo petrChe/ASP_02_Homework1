0aa2df1808c123cf013d538646da626f917570c8
Petr <Petr@PETR->
2016-08-18 17:12:50 +0300

    Add project files.

---
+++ ASP_02_Homework1/PersonCheckForm.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PersonCheckForm.aspx.cs" Inherits="ASP_02_Homework1.PersonCheckForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Write the text from the image
+        <br />
+        <br />
+        <asp:Image ID="Image1" runat="server" Height="213px" Width="204px" />
+        <br />
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Check Person"/>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ASP_02_Homework1/PersonCheckForm.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+
+namespace ASP_02_Homework1
+{
+    public partial class PersonCheckForm : System.Web.UI.Page
+    {
+        Dictionary<int, string> images = new Dictionary<int, string>();
+
+        protected override void OnPreInit(EventArgs e)
+        {
+            images.Add(123, "~/Images/Chrysanthemum.jpg");
+            images.Add(456, "~/Images/Hydrangeas.jpg");
+            images.Add(789, "~/Images/Tulips.jpg");
+            base.OnPreInit(e);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            Image1.ImageUrl = RandomValues(images);
+        }
+
+        public string RandomValues(Dictionary<int, string> dict)
+        {
+            List<string> values = Enumerable.ToList(dict.Values);
+            Random rand = new Random();
+            int randValue = rand.Next(values.Count);
+            return values[randValue];
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var imageKey = images.FirstOrDefault(x => x.Value == Image1.ImageUrl).Key;
+            //var imageKey = images.Where(x => x.Value == Image1.ImageUrl).Select(x => x.Key);
+            if (TextBox1.Text == (imageKey).ToString())
+            {
+                MessageBox.Show("Person is real");
+            }
+            else
+            {
+                MessageBox.Show("Person is fake");
+            }
+        }
+    }
+}
+
